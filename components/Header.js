4a2816811457a91421dd2a035e0c4187cc80ae17
karami4yasser <karamiyassir2000@gmail.com>
2022-05-23 00:38:00 +0100

    ES-20:navbar'

--- components/Header.js
+++ components/Header.js
@@ -12,33 +12,20 @@ import {
 } from "@heroicons/react/outline";
 import Image from "next/image";
 import HeaderItem from "./HeaderItem";
-import { useStateValues } from "./StateProvider";
 import { useRouter } from "next/router";
-import { auth } from "./firebase";
 import { useHistory } from "react-router-dom";
 //import ShoppingBasketIcon from "@mui/icons-material/ShoppingBasket";
-import { signOut } from "firebase/auth";
 
 function Header() {
-  const [{ basket, user,username }, dispatch] = useStateValues();
   const router = useRouter();
   const history = useHistory();
   const handleAuth = () => {
-    if (user) {
-
-     signOut(auth);
-    } else {
-      router.push("/login");
-    }
+    router.push("/login");
   };
 
   return (
-    <header
-      className="flex flex-col xl:flex-row m-5 justify-between items-center h-auto  "
-    >
-      <div
-        className="flex flex-grow justify-evenly max-w-2xl"
-      >
+    <header className="flex flex-col xl:flex-row m-5 justify-between items-center h-auto  ">
+      <div className="flex flex-grow justify-evenly max-w-2xl">
         <HeaderItem
           title="HOME"
           Icon={HomeIcon}
@@ -49,24 +36,10 @@ function Header() {
         <HeaderItem title="SEARCH" Icon={SearchCircleIcon} />
 
         <HeaderItem
-        className="pt-1"
-          title={user ? username : "Sign In"}
+          className="pt-1"
+          title={"Sign In"}
           Icon={UserIcon}
           Function={handleAuth}
-        />
-        {user ? <HeaderItem
-          title={ "Orders"}
-          Icon={ShoppingBagIcon}
-          Function={() => {
-            user? router.push("/orders"): console.log('');
-          }}
-        />      :console.log('') }
-        <HeaderItem
-          title={basket?.length}
-          Icon={ShoppingCartIcon}
-          Function={() => {
-            router.push("/checkout");
-          }}
         />
       </div>
 
